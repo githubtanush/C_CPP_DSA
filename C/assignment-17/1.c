4dbe6e867da3493d0ebff229cd17a728b0d71def
Tanush Arora <tanush935@gmail.com>
2023-12-16 15:00:01 +0530

    Second commit

--- C/assignment-17/1.c
+++ C/assignment-17/1.c
@@ -8,22 +8,81 @@
 // }
 
 
-//programme to calculate the length of a string 
-#include<stdio.h>
+// //programme to calculate the length of a string 
+// #include<stdio.h>
 // int length(char s[]){
-//     int i ;
-//     for(int i = 0; s[i];i++)
-//         return i;
-    
+//     int i;
+//     for(i=0;s[i];i++);
+//         return i; 
 // }
+// int main(){
+//     char str[1000];
+//     fgets(str,1000,stdin);
+//     printf("%s",str);
+//     int count = length(str);
+//     printf("The length of the string is %d",count);
+//     return 0;
+// }
+
+
+
+
+//reverse a string 
+// #include<stdio.h>
+// int length(char str[]){
+//     int i;
+//     for(i=0;str[i];i++);
+//         return i; 
+// }
+
+// // //Method 1 logic with one more array 
+// // char* reverse(char str[]){
+// //     char temp[1000];
+// //     int i,l,j;
+// //     l=length(str);
+// //     for(i=l-1,j=0;i--;j++)
+// //         temp[j]=str[i];
+// //     for(i=0;str[i];i++)
+// //         str[i]=temp[i];
+// //     return str;
+// // }
+
+// // Method 2 : optimised logic without use of 2nd string array
+// char* reverse(char str[]){
+//     int i,l;
+//     char t;
+//     l=length(str);
+//     for(i=0; i < l/2; i++){
+//         t = str[i];
+//         str[i]=str[l-1-i];
+//         str[l-i-1]=t;
+//     }
+//     return str;
+
+// }
+// int main(){
+//     char str[1000];
+//     fgets(str,1000,stdin);
+//     printf("Before Reverse : %s",str);
+//     printf("After Reverse : %s",reverse(str));
+//     getchar();
+//     return 0;
+// }
+
+
+//Write a function to Convert a string to uppercase
+#include<stdio.h>
+char* uppercase(char str[]){
+    for(int i = 0; str[i];i++){
+        if(str[i]>='a' && str[i]<='z'){
+            str[i]=str[i]-32;
+        }
+    }
+    return str;
+}
 int main(){
-    char s[1000];
-    fgets(s,30,stdin);
-    // int m = length(s);
-    // printf("%d",m);
-    int i ;
-    for(int i = 0; s[i];i++)
-        return i;
-    getchar();
-    return 0;
+    char str[1000];
+    fgets(str,1000,stdin);
+    printf("Before converting to uppercase : %s",str);
+    printf("After converting to uppercase : %s",uppercase(str));
 }
