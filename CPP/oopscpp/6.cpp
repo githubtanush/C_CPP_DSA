ef616742f5cef01c57a089c97e140f793e7f7d92
Tanush Arora <tanush935@gmail.com>
2023-12-23 03:58:22 +0530

    twentytwo commit

---
+++ CPP/oopscpp/6.cpp
@@ -0,0 +1,14 @@
+//Assignment - 13
+#include<iostream>
+using namespace std;
+class fraction{
+    private:
+    long numerator;
+    long denominator;
+
+    public:
+        fraction(long n = 0, long d = 0);
+};
+int main(){
+    return 0;
+}
