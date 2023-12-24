b91ce76e7ec21f37649590b220aa0f0c42a1e55e
Tanush Arora <tanush935@gmail.com>
2023-12-24 16:01:07 +0530

    twentytwo commit

--- CPP/oopscpp/lecturesmysirg/six.cpp
+++ CPP/oopscpp/lecturesmysirg/six.cpp
@@ -1,3 +1,4 @@
+// //Inheritance
 // #include<iostream>
 // #include<string.h>
 // using namespace std;
@@ -26,16 +27,39 @@
 //     private:
 //         float salary;
 //     protected:
-//         void salary(float s ){
+//         void setSalary(float s ){
 //             salary = s;
 //         }
 //     public:
 //         float getSalary(){return salary;}
 // };
-// class UpdateEmployee:public Employee{
+// // //Version 1
+// // void setEmployee(Employee &e, char n[],int a, float s){
+// //  e.getAge();//access only public member of the class 
+// //  //so not possible 
+// //  //why ?? because user 1 can access only public member of the class
+// // }
 
+// //Version 2
+// class UpdateEmployee:public Employee{
+//         //now in this you can access also protected members and variables
+//         public:
+//             void setEmployee(char n[],int a , float s){
+//                 //name cannot access here because name is the private member of person class.
+//                 // strcpy(name,n);
+//                 setName(n);
+//                 setage(a);
+//                 setSalary(s);
+//             }
+//             void showEmployee(){
+//                 cout<<getname()<<" "<<getAge()<<" "<<getSalary();
+//             }
 // };
 // int main(){
-
+//     UpdateEmployee emp;
+//     char s[]="Amit";
+//     emp.setEmployee(s,29,4000);
+//     emp.showEmployee();
+//     cout<<endl;
 //     return 0;
 // }
