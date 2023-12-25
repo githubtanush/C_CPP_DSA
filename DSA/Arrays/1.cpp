f43a23b4072525e35799bfdf2344a63c0d0cd352
Tanush Arora <tanush935@gmail.com>
2023-12-26 01:58:41 +0530

    twentysix commit

--- DSA/Arrays/1.cpp
+++ DSA/Arrays/1.cpp
@@ -1,122 +1,127 @@
+//Array 
 #include<iostream>
 using namespace std;
 class Array{
     private:
         int capacity;
-        int last_index;
         int *ptr;
+        int lastIndex;
     
     public:
-    //functions bda hai complicated hai loop use ho rah hai variable hai to membership label lga kar bahar bnao yeh 
-    //better esliye hai kyuki hme inline nhi bnana functions ko or class mein by default inline bnta hai  
-    Array(int);
-    bool isEmpty();
-    void append(int);
-    bool isFull();
-    void insert(int,int);
-    void editElement(int,int);
-    void del(int);
-    int getItem(int);
-    int count();
-     ~Array();
-    int find(int);
+        Array(int size);
+        bool isEmpty();
+        bool isFull();
+        void append(int);
+        void insert(int,int);
+        void editElement(int,int);
+        void del(int);
+        int getElement(int);
+        void show();
+        int count();
+        ~Array();
+        int find(int);
 };
 Array::Array(int size){
-        if(size>0){
-        capacity = size;
-        //Capacity aayga na ptr mein toh chay size brabar hi hai lekin dynamic array to capacity se hi bn rah hai na 
-        ptr = new int[capacity];
-        last_index = -1;
-        }
-        else{
-            ptr=NULL;
-        }
-     }
+    if(size>0){
+    capacity = size;
+    lastIndex = -1;
+    ptr = new int[capacity];
+    }
+    else   
+        ptr=NULL;
+}
 bool Array::isEmpty(){
-    return last_index==-1;
+    return lastIndex==-1;
 }
 bool Array::isFull(){
-    return last_index==capacity;
+    return lastIndex==capacity-1;
 }
 void Array::append(int data){
     if(isFull()){
         cout<<"\nOverflow";
     }
     else{
-        last_index++;
-        ptr[last_index]=data;
+        lastIndex++;
+        ptr[lastIndex]=data;
     }
 }
-void Array::insert(int index, int data){
-    if(index<0 || index>last_index+1){
-        cout<<"\nInvalid Index";
+void Array::insert(int index,int data){
+    if(index<0 || index>lastIndex+1){
+        cout<<"\nInvalid position";
     }
     else if(isFull()){
         cout<<"\nOverflow";
     }
     else{
-        for(int i = last_index;i>=index; i--){
+        for(int i = lastIndex; i>=index;i-- ){
             ptr[i+1]=ptr[i];
         }
         ptr[index]=data;
-        last_index++;
+        lastIndex++;
     }
 }
-void Array::editElement(int index, int data){
-    if(index<0 || index>last_index){
-        cout<<"\nInvalid Index or Empty Index";
+void Array::editElement(int index,int data){
+    if(index<0 || index>lastIndex){
+        cout<<"\nInvalid Index or empty array";
     }
     else{
         ptr[index]=data;
     }
 }
 void Array::del(int index){
-    if(index<0 || index>last_index){
-        cout<<"\nInvalid index or empty array";
+    if(index<0 || index>lastIndex){
+        cout<<"\nInvalid Index or empty array";
     }
     else{
-        for(int i = index; i < last_index; i++ ){
+        for(int i = index;i<lastIndex;i++){
             ptr[i]=ptr[i+1];
-            last_index--;
         }
+        lastIndex--;
     }
 }
-int Array::getItem(int index){
-        if(index<0 || index>last_index){
-            cout<<"\n Invalid index or empty array";
-            //So when we use the exception handling then it becomes the good version of the code;
-            throw new exception();
-            //we don't write try catch in this get item prefer this because that is user's decision that what they 
-            //want to print
-            //if these condition is false return not working compiler thinks in early binding so also 
-            //return something after the int block
-        }
+void Array::show(){
+    for(int i = 0; i <=lastIndex;i++)
+        cout<<i<<" "<<ptr[i]<<endl;
+}
+int Array::getElement(int index){
+    if(index<0 || index>lastIndex){
+        cout<<"\nInvalid Index or empty array";
+        throw new exception();
+    }
+    return ptr[index];
 
-        //do robust programming because we can train for all the situations
-        //Java is mainly develop for robust programming that's why software are good when enable in java
-        //but in cpp they introduce the concept of exception handling
-        return ptr[index];
-        //error to nhi aaygi par return chlna to pkka ho gya na //means this is wrong.
-        //in future this line is become the bug return vali
 }
 int Array::count(){
-    return last_index+1;
+    return lastIndex+1;
 }
 Array::~Array(){
     delete []ptr;
+    ptr = NULL;
 }
 int Array::find(int data){
-    int i;
-    for(int i = 0; i < last_index;i++){
+    for(int i = 0; i <= lastIndex;i++){
         if(ptr[i]==data){
             return i;
         }
     }
     return -1;
-}    
+}
 
 int main(){
     Array a1(8);
-    a1.insert(2,5);
+    a1.append(4);
+    a1.append(3);
+    a1.append(2);
+    a1.append(8);
+    a1.append(9);
+    a1.append(6);
+    a1.append(5);
+    a1.append(7);
+    bool p = a1.isEmpty();
+    cout<<p<<endl;
+    a1.editElement(2,4);
+    a1.show();
+    int m = a1.getElement(5);
+    cout<<endl<<m;
     return 0;
 }
