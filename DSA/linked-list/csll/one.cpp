6f07763f60cad3bb96949738f4177d54c703cedf
Tanush Arora <tanush935@gmail.com>
2024-01-06 22:47:58 +0530

    fourty commit

--- DSA/linked-list/csll/one.cpp
+++ DSA/linked-list/csll/one.cpp
@@ -2,26 +2,31 @@
 using namespace std;
 struct node{
     int item;
-    node* next;
+    node *next;
 };
 class CLL{
     private:
         node *last;
+    
     public:
         CLL();
         void insertAtStart(int);
         void insertAtLast(int);
         node* search(int);
+        void show();
+
+
 };
 CLL::CLL(){
     last = NULL;
 }
 void CLL::insertAtStart(int data){
     node *n;
+    n = new node;
     n->item = data;
     if(last==NULL){
-        n->next = n;
-        last = n;
+    n->next = n;
+    last = n;
     }
     else{
         n->next = last->next;
@@ -32,7 +37,7 @@ void CLL::insertAtLast(int data){
     node *n;
     n = new node;
     n->item = data;
-    if(last==NULL){
+    if(last == NULL){
         n->next = n;
         last = n;
     }
@@ -46,17 +51,38 @@ node* CLL::search(int data){
     node *t;
     if(last){
         t = last->next;
-        do
-        {
-            if(t->item = data){
-                return t;
-            }
-            t = t->next;
-        }while(t!=last->next);
+    
+    do{
+        if(t->item==data)
+            return t;
+        t = t->next;
+    }while(t!=last->next);
+    }
+    return NULL;
 }
-return NULL;
+void CLL::show() {
+    if (last == nullptr) {
+        cout << "Empty list" << endl;
+        return;
+    }
+
+    node *t = last->next;
+    do{
+        cout << t->item << "->";
+        t = t->next;
+    } while(t!=last->next);
+    cout << endl;
 }
 
 int main(){
-
+    CLL c;
+    c.insertAtStart(67);
+    c.insertAtStart(43);
+    c.insertAtStart(21);
+    c.insertAtStart(98);
+    c.insertAtLast(28);
+    c.insertAtLast(32);
+    c.insertAtLast(56);
+    c.show();
+    return 0;
 }
--- DSA/linked-list/dll/1.cpp
+++ DSA/linked-list/dll/1.cpp
@@ -1,27 +1,182 @@
+// #include<iostream>
+// using namespace std;
+// struct node{
+//     int item;
+//     node* prev;
+//     node* next;
+// };
+// class DLL{
+//     private:
+//         node* start;
+//     public:
+//         DLL();
+//         void insertAtStart(int);
+//         void insertAtLast(int);
+//         node* search(int);
+//         void insertAtSpecific(node*,int);
+//         void deleteFirst();
+//         void deleteLast();
+//         void deleteSpecific(int data);
+//         void show();
+//         ~DLL();
+// };
+// DLL::DLL(){
+//     start=NULL;
+// }
+// void DLL::insertAtStart(int data){
+//     node *n;
+//     n = new node;
+//     n->prev = NULL;
+//     n->item = data;
+//     n->next = start;
+//     if(start)
+//         start->prev = n;
+//     start  = n;
+// }
+// void DLL::insertAtLast(int data){
+//     node *n,*t;
+//         n = new node;
+//         n->item = data;
+//         n->next = NULL;
+//         if(start == NULL){
+//             n->prev = NULL;
+//             start = n;
+//         }
+//         else{
+//             t = start;
+//             while(t->next!=NULL){
+//                 t = t->next;  
+//             }
+//             n->prev = t;
+//             t->next = n;
+//         }     
+// }
+// node* DLL::search(int data){
+//     node *t;
+//     t = start;
+//     while(t!=NULL){
+//         if(t->item==data)
+//             return t;
+//         t = t->next;
+//     }
+//     return NULL;
+// }
+// void DLL::insertAtSpecific(node* temp,int data){
+//     node *n,*t;
+//     n = new node;
+//     n->prev = temp;
+//     n->item = data;
+//     n->next = temp->next;
+//     if(temp->next!=NULL)
+//         temp->next->prev = n;
+//     temp->next = n;
+// }
+// void DLL::show(){
+//     node *t;
+//     t = start;
+//     while(t!=NULL){
+//         cout<<"<-"<<t->item<<"->";
+//         t = t->next;
+//     }
+// }
+// void DLL::deleteFirst(){
+//     node *t;
+//     t = start;
+//     if(start){
+//         if(start->next==NULL){
+//             delete start;
+//             start = NULL;
+//         }
+//         else{
+//             start->next->prev = NULL;
+//             start = start->next;
+//             delete t;
+//         }
+//     }
+// }
+// void DLL::deleteLast(){
+//     node *t;
+//     if(start){
+//         if(start->next==NULL){
+//             delete start;
+//             start = NULL;
+//         }
+//         else{
+//             t = start;
+//             while(t->next!=NULL){
+//                 t = t->next;
+//             }
+//             t->prev->next = NULL;
+//             delete t;
+//         }
+//     }
+// }
+// void DLL::deleteSpecific(int data){
+//     node *t;
+//     t = start;
+//     while(t){
+//         if(t->item==data)
+//         {
+//             if(t->next != NULL)
+//                 t->next->prev = t->prev;
+                
+//             if(t->prev != NULL)
+//                 t->prev->next = t->next;
+                
+//             if(t->prev == NULL)
+//                 start = t->next;
+
+//             delete t;
+//             break;
+//         }
+//         t = t->next;
+//     }
+// }
+// DLL::~DLL(){
+//     while(start)
+//         deleteFirst();
+// }
+// int main(){
+//     DLL d;
+//     d.insertAtStart(8);
+//     d.insertAtStart(87);
+//     d.insertAtStart(34);
+//     d.insertAtLast(67);
+//     d.insertAtLast(32);
+//     node *d1 = d.search(34);
+//     cout<<d1<<"         ";
+//     d.deleteFirst();
+//     d.deleteLast();
+//     d.deleteSpecific(8);
+//     d.show();
+//     return 0;
+// }
+
+
 #include<iostream>
 using namespace std;
 struct node{
+    node *prev;
     int item;
-    node* prev;
-    node* next;
+    node *next;
 };
 class DLL{
     private:
-        node* start;
+        node *start;
     public:
-        DLL();
-        void insertAtStart(int);
-        void insertAtLast(int);
-        node* search(int);
-        void insertAtSpecific(node*,int);
-        void deleteFirst();
-        void deleteLast();
-        void deleteSpecific(int data);
-        void show();
-        ~DLL();
+    DLL();
+    void insertAtStart(int);
+    void insertAtLast(int);
+    node* search(int);
+    void insertAtpos(node*,int);
+    void show();
+    void deletefirst();
+    void deleteLast();
+    void deleteatpos(int);
+    ~DLL();
 };
 DLL::DLL(){
-    start=NULL;
+    start = NULL;
 }
 void DLL::insertAtStart(int data){
     node *n;
@@ -31,123 +186,132 @@ void DLL::insertAtStart(int data){
     n->next = start;
     if(start)
         start->prev = n;
-    start  = n;
+    start = n;
 }
 void DLL::insertAtLast(int data){
     node *n,*t;
-        n = new node;
-        n->item = data;
-        n->next = NULL;
-        if(start == NULL){
-            n->prev = NULL;
-            start = n;
-        }
-        else{
-            t = start;
-            while(t->next!=NULL){
-                t = t->next;  
-            }
-            n->prev = t;
-            t->next = n;
-        }     
-}
-node* DLL::search(int data){
-    node *t;
-    t = start;
-    while(t!=NULL){
-        if(t->item==data)
-            return t;
-        t = t->next;
-    }
-    return NULL;
-}
-void DLL::insertAtSpecific(node* temp,int data){
-    node *n,*t;
     n = new node;
-    n->prev = temp;
     n->item = data;
-    n->next = temp->next;
-    if(temp->next!=NULL)
-        temp->next->prev = n;
-    temp->next = n;
+    n->next = NULL;
+    if(start==NULL){
+        n->prev = NULL;
+        start = n;
+    }
+    else{
+        t = start;
+        while(t->next!=NULL){
+            t = t->next;
+        }
+        t->next = n;
+        n->prev = t;
+    }
 }
 void DLL::show(){
     node *t;
     t = start;
     while(t!=NULL){
-        cout<<"<-"<<t->item<<"->";
+        cout<<t->item<<"->";
         t = t->next;
     }
 }
-void DLL::deleteFirst(){
+node* DLL::search(int data){
     node *t;
     t = start;
+    while(t!=NULL){
+        if(t->item == data)
+            return t;
+        t = t->next;
+    }
+    return NULL;
+}
+void DLL::insertAtpos(node *temp,int data){
+    node *n;
+    if(temp){
+        n = new node;
+        n->item = data;
+        n->next = temp->next;
+        n->prev = temp;
+        if(temp->next!=NULL){
+            temp->next->prev = n;
+        }
+        temp->next = n;
+    }
+}
+void DLL::deletefirst(){
+    node *t;
     if(start){
         if(start->next==NULL){
             delete start;
             start = NULL;
         }
         else{
-            start->next->prev = NULL;
-            start = start->next;
-            delete t;
+        t = start;
+        start->next->prev = NULL;
+        start = start->next;
+        delete t;
         }
     }
 }
 void DLL::deleteLast(){
-    node *t;
-    if(start){
-        if(start->next==NULL){
-            delete start;
-            start = NULL;
-        }
-        else{
-            t = start;
-            while(t->next!=NULL){
-                t = t->next;
+        node *t;
+        if(start){
+            if(start->next == NULL){
+                delete start;
+                start = NULL;
             }
-            t->prev->next = NULL;
-            delete t;
-        }
-    }
+            else{
+                t = start;
+                while(t->next!=NULL){
+                    t = t->next;
+                }
+                t->prev->next = NULL;
+                delete t;
+            }
+            
+      }
 }
-void DLL::deleteSpecific(int data){
+void DLL::deleteatpos(int data){
     node *t;
     t = start;
     while(t){
-        if(t->item==data)
-        {
-            if(t->next != NULL)
+        if(t->item == data){
+            if(t->next!=NULL){
                 t->next->prev = t->prev;
-                
-            if(t->prev != NULL)
+            }
+            if(t->prev!=NULL){
                 t->prev->next = t->next;
-                
-            if(t->prev == NULL)
+            }
+            if(t->prev==NULL){
                 start = t->next;
-
-            delete t;
-            break;
+                delete t;
+                break;
+            }
         }
         t = t->next;
     }
 }
 DLL::~DLL(){
     while(start)
-        deleteFirst();
+        deletefirst();
 }
 int main(){
     DLL d;
-    d.insertAtStart(8);
-    d.insertAtStart(87);
-    d.insertAtStart(34);
-    d.insertAtLast(67);
-    d.insertAtLast(32);
-    node *d1 = d.search(34);
-    cout<<d1<<"         ";
-    d.deleteFirst();
+    d.insertAtStart(76);
+    d.insertAtStart(32);
+    d.insertAtStart(89);
+    d.insertAtStart(28);
+    d.insertAtLast(34);
+    d.insertAtLast(23);
+    d.insertAtLast(21);
+    d.insertAtLast(10);
+    node *f = d.search(89);
+
+    cout<<f<<"       ";
+    d.insertAtpos(f,56);
+    d.deletefirst();
     d.deleteLast();
-    d.deleteSpecific(8);
+    d.deleteatpos(76);
     d.show();
+
     return 0;
 }
