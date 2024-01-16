00bf7a575cd982db801cea95a7db156782a9739f
Tanush Arora <tanush935@gmail.com>
2024-01-16 22:49:20 +0530

    fourtyseven commit

--- CPP/oopscpp/7.cpp
+++ CPP/oopscpp/7.cpp
@@ -1,51 +1,73 @@
-//Assignment = 14
+// //Assignment = 14
+// #include<iostream>
+// using namespace std;
+// class Complex{
+//     private:
+//         int a,b;
+    
+//     public: 
+//         Complex(int x,int y){
+//             a = x;
+//             b = y;
+//         }
+//         void showData(){
+//             cout<<"a = "<<a <<" b = "<<b<<"i";
+//         }
+//         friend Complex operator+(Complex,Complex);
+//         friend Complex operator-(Complex,Complex);
+//         friend Complex operator*(Complex,Complex);
+        
+// };
+// Complex operator+(Complex t1,Complex t2){
+//     Complex temp(0,0);
+//     temp.a = t1.a + t2.a;
+//     temp.b = t1.b + t2.b;
+//     return temp;
+// }
+// Complex operator-(Complex t1,Complex t2){
+//     Complex temp(0,0);
+//     temp.a = t1.a - t2.a;
+//     temp.b = t1.b - t2.b;
+//     return temp;
+// }
+// Complex operator*(Complex t1,Complex t2){
+//     Complex temp(0,0);
+//     temp.a = t1.a*t2.a - t1.b*t2.b;
+//     temp.b = t1.a*t2.b + t2.a*t1.b;
+//     return temp;
+// }
+// int main(){
+//     Complex c1(3,4),c2(5,7),c3(0,0),c4(0,0),c5(0,0);
+//     c3 = c1 + c2;
+//     c4 = c1 - c2;
+//     c5 = c1 * c2;
+//     cout<<endl;
+//     c3.showData();
+//     cout<<endl;
+//     c4.showData();
+//     cout<<endl;
+//     c5.showData();
+//     return 0;
+// }
+
+
+
 #include<iostream>
 using namespace std;
 class Complex{
     private:
         int a,b;
-    
-    public: 
-        Complex(int x,int y){
+    public:
+        void setData(int x,int y){
             a = x;
             b = y;
         }
-        void showData(){
-            cout<<"a = "<<a <<" b = "<<b<<"i";
+        void printData(){
+            cout<<" a = "<< a << " b = "<<b<<endl;
         }
-        friend Complex operator+(Complex,Complex);
-        friend Complex operator-(Complex,Complex);
-        friend Complex operator*(Complex,Complex);
-        
 };
-Complex operator+(Complex t1,Complex t2){
-    Complex temp(0,0);
-    temp.a = t1.a + t2.a;
-    temp.b = t1.b + t2.b;
-    return temp;
-}
-Complex operator-(Complex t1,Complex t2){
-    Complex temp(0,0);
-    temp.a = t1.a - t2.a;
-    temp.b = t1.b - t2.b;
-    return temp;
-}
-Complex operator*(Complex t1,Complex t2){
-    Complex temp(0,0);
-    temp.a = t1.a*t2.a - t1.b*t2.b;
-    temp.b = t1.a*t2.b + t2.a*t1.b;
-    return temp;
-}
 int main(){
-    Complex c1(3,4),c2(5,7),c3(0,0),c4(0,0),c5(0,0);
-    c3 = c1 + c2;
-    c4 = c1 - c2;
-    c5 = c1 * c2;
-    cout<<endl;
-    c3.showData();
-    cout<<endl;
-    c4.showData();
-    cout<<endl;
-    c5.showData();
-    return 0;
+    Complex x;
+    x.setData(7,8);
+    x.printData();
 }
